47a308ee0e739b9cbf0d60c7749e14c812bfc220
Marvin Löbel <loebel.marvin@gmail.com>
2018-06-12 16:53:40 +0200

    attempt to test memory tracking

--- test/tudostats.cpp
+++ test/tudostats.cpp
@@ -2,8 +2,49 @@
 
 #include <StatPhase.hpp>
 
+#include <memory>
+
 using namespace tdc;
 
-TEST(Tudostats, test0) {
+TEST(Tudostats, empty_phase) {
+    tdc::StatPhase root("Root");
+    {
+        auto x = std::make_unique<char[]>(128);
+        for (size_t i = 0; i < 128; i++) {
+            x[i] = i;
+            std::cout << int(x[i]) << ",";
+        }
+        std::cout << std::endl;
+        auto y = malloc(128);
+        *(char*)y = 128;
+        free(y);
+    }
+    root.to_json().str(std::cout);
+    std::cout << std::endl;
+}
 
+TEST(Tudostats, subphase_1) {
+    tdc::StatPhase root("Root");
+    {
+        tdc::StatPhase sub1("sub1");
+        {
+            auto x = std::make_unique<char[]>(128);
+            for (size_t i = 0; i < 128; i++) {
+                x[i] = i;
+                std::cout << int(x[i]) << ",";
+            }
+            std::cout << std::endl;
+        }
+        sub1.split("sub2");
+        {
+            auto x = std::make_unique<char[]>(128);
+            for (size_t i = 0; i < 128; i++) {
+                x[i] = i;
+                std::cout << int(x[i]) << ",";
+            }
+            std::cout << std::endl;
+        }
+    }
+    root.to_json().str(std::cout);
+    std::cout << std::endl;
 }
